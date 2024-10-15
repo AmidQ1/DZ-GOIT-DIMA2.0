5c8832f8074abb1fb46b246c330e105c62dc29b4
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-10-15 23:24:04 +0300

    фвв

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const cub = document.querySelector(".cub")
+// const cub = document.querySelector(".cub")
 
 
 
-function getBgColor(){
-    const red = Math.round(Math.random()*255);
-    const blue = Math.round(Math.random()*255);
-    const green = Math.round(Math.random()*255);
+// function getBgColor(){
+//     const red = Math.round(Math.random()*255);
+//     const blue = Math.round(Math.random()*255);
+//     const green = Math.round(Math.random()*255);
 
-    return `rgb(${red}, ${green}, ${blue})`
-}
+//     return `rgb(${red}, ${green}, ${blue})`
+// }
 
-// rgb(123, 45, 68)
+// // rgb(123, 45, 68)
 
-setInterval(() => {
-    cub.style.background = getBgColor();
-},3000);
+// setInterval(() => {
+//     cub.style.background = getBgColor();
+// },3000);
 
-// 3000
+// // 3000
+
+const happy = new Date("11-5-2007");
+console.log(happy.getFullYear());
+console.log(happy.getMonth());
+console.log(happy.getDate());
+console.log(happy.getHours());
+console.log(happy.getMinutes());
+console.log(happy.getSeconds());
