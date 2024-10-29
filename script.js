08938ed6e1451f52b9038af77bc588f7842df4b1
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-10-29 20:10:26 +0200

    фвв

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// const cub = document.querySelector(".cub")
+// Використовуючи Promise.reject, створіть Promise, який завершується з помилкою.
 
+// const pr1 = new Promise(( _, reject) => {
+//   reject("error")
+// });
 
+const pr2 = Promise.reject("error")
 
-// function getBgColor(){
-//     const red = Math.round(Math.random()*255);
-//     const blue = Math.round(Math.random()*255);
-//     const green = Math.round(Math.random()*255);
-
-//     return `rgb(${red}, ${green}, ${blue})`
-// }
-
-// // rgb(123, 45, 68)
-
-// setInterval(() => {
-//     cub.style.background = getBgColor();
-// },3000);
-
-// // 3000
-
-// const happy = new Date("11-5-2007");
-// console.log(happy.getFullYear());
-// console.log(happy.getMonth());
-// console.log(happy.getDate());
-// console.log(happy.getHours());
-// console.log(happy.getMinutes());
-// console.log(happy.getSeconds());
-
-
-let a1 = new Promise(function(resolve, reject){
-    resolve(5)
+pr2.catch((error) => {
+  console.log("erro", error)
 })
