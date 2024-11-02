189c8b517b680a41873b5d75e2ad7e11ec17f7c2
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-11-02 17:46:36 +0200

    REWORKED its task

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sliderInput = document.querySelector('.slider__input');
-    const sliderImage = document.querySelector('.slider__image');
-  
-    
-    const resizeImage = _.debounce(function (event) {
-      const scale = event.target.value / 100; 
-      sliderImage.style.transform = `scale(${scale})`; 
-    }, 300); 
-  
-    sliderInput.addEventListener('input', resizeImage);
-  });
+// task 1
+const sliderInp = document.querySelector(".slider__input");
+const sliderImg = document.querySelector(".slider__img");
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const box = document.getElementById('box');
-  
+const sizeImg = _.throttle(function (e) {
+  const scale = e.target.value / 100;
+  sliderImg.style.transform = `scale(${scale})`;
+}, 300);
 
-    const moveBox = _.debounce(function (event) {
-      box.style.left = event.pageX + 'px';
-      box.style.top = event.pageY + 'px';
-    }, 100); 
-  
+sliderInp.addEventListener("input", sizeImg);
 
-    document.addEventListener('mousemove', moveBox);
-  });
-   
+// task 2
+const box = document.querySelector(".box");
+
+const moveBox = _.throttle(function (e) {
+  box.style.left = e.pageX + "px";
+  box.style.top = e.pageY + 50 + "px";
+  // Я додав до висоти 50 для того, щоб не заважала для першого завдання 
+}, 100);
+
+document.addEventListener("mousemove", moveBox);
