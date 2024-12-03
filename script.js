808cc0c88bd463578155350a91564f6ef1f8ab5f
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-12-03 18:20:20 +0200

    add

--- script.js
+++ script.js
@@ -26,21 +26,46 @@
 
 
 
-// https://newsapi.org/v2/everything?
-const params = new URLSearchParams({
-  apiKey: `7ad8b71dda2643768ec2ca3287e4b976`,
-  page: 2,
-  pageSize: 3,
-  q: `programming`,
-});
+// // https://newsapi.org/v2/everything?
+// const params = new URLSearchParams({
+//   apiKey: `7ad8b71dda2643768ec2ca3287e4b976`,
+//   page: 2,
+//   pageSize: 3,
+//   q: `programming`,
+// });
 
-const request = fetch(`https://newsapi.org/v2/everything?${params.toString()}`);
-const articles = request
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// const request = fetch(`https://newsapi.org/v2/everything?${params.toString()}`);
+// const articles = request
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
 
-  document.
+
+var scanf = require('scanf');
+
+var n = scanf('%d');
+var a = scanf('%d');
+
+var f = false;
+
+for(var i = 0; i<n && !f; i++){
+    for(var j=i+1; j<=n && !f; j++){
+        var substring = a.substring(i, j)
+        var t=0
+        for(var k = 0; k<z.length; k++){
+            if(z[k]==='1'){
+                t++
+            }
+        }
+    }
+
+    var q= z.length
+    if(t>=2 && q%t===0){
+        console.log(i+1, j)
+        f = true
+    }
+
+}
